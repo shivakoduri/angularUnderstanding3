ce909f06652fde7989ae496975ffa22e53fe32d3
shiva <1917007+shivakoduri@users.noreply.github.com>
2018-05-13 10:27:06 +0530

    Using Local References in Templates

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -16,7 +16,8 @@ export class CockpitComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAddServer() {
+  onAddServer(nameInput) {
+    console.log(nameInput.value);
     this.serverCreated.emit({
       serverName: this.newServerName,
       serverContent: this.newServerContent
